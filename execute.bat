f9932cfaaf22a6a108e2744b934d2fc9c1821dc8
Z1KO <Z1KOxl@pm.me>
2024-05-20 16:43:10 +0200

    Enhanced execute script
    Enhanced script to verify existence of main.py in src directory.

--- execute.bat
+++ execute.bat
@@ -1,4 +1,12 @@
 @echo off
 
+rem Change directory to the location of the python script
 cd src
+if errorlevel 1 (
+    echo Failed to change directory to src. Exiting...
+    ping 127.0.0.1 -n 3 > nul
+    exit
+)
+
+rem Execute python script
 python main.py
