971b85871b565697199eacc7e72de53773d5e6c4
Z1KO <Z1KOxl@pm.me>
2024-05-20 15:32:20 +0200

    Added various functions
    - Renamed some functions and variables.

--- src/main.py
+++ src/main.py
@@ -8,23 +8,37 @@ def clear_console():
     os.system(command)
 
 
-def play_guessing_game(tries: int, TARGET_NUMBER: int) -> bool:
+def getValidInput(prompt: str) -> int:
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Invalid input. please enter a valid number.")
+
+
+def compareGuess(guess: int, TARGET_NUMBER: int) -> bool:
+    if guess < TARGET_NUMBER:
+        print(f"Your number {guess} is lower than the target number")
+    elif guess > TARGET_NUMBER:
+        print(f"Your number {guess} is higher than the target number")
+    else:
+        return True
+    
+    return False
+
+
+def guessingGame(tries: int, TARGET_NUMBER: int) -> bool:
     print("Guess a number between 1 and 25")
 
     # Loop until all tries are used
     while tries:
         print(f"Your current tries [{tries}]")
-        guess = int(input("Guess number: "))
+        guess = getValidInput("Guess number: ")
         print() # Print endline after user input
 
-        # Compare the user's guess with the target number
-        if guess < TARGET_NUMBER:
-            print(f"Your number {guess} is higher than the target number")
-        elif guess > TARGET_NUMBER:
-            print(f"Your number {guess} is lower than the target number")
-        else:
+        if compareGuess(guess, TARGET_NUMBER):
             return True
-    
+
         tries -= 1
 
     # Return False if all tries have been used up without guessing the correct number
@@ -35,14 +49,14 @@ def main():
     tries: int = 5
     TARGET_NUMBER: int = random.randint(1, 25)
 
-    found = play_guessing_game(tries, TARGET_NUMBER)
+    found = guessingGame(tries, TARGET_NUMBER)
 
     clear_console()
     print(f"The target number was [{TARGET_NUMBER}]")
 
     # Ternary operator
-    resultPromt = "Congratulations, you guessed correctly!\n" if found else "Good luck next time!\n"
-    print(resultPromt)
+    resultPrompt = "Congratulations, you guessed correctly!\n" if found else "Good luck next time!\n"
+    print(resultPrompt)
 
     os.system('pause')
 
